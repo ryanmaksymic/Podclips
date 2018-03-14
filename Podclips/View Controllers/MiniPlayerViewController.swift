8e98c1ce40aae00b6f6e610f25c795262f8b4a4e
ryanmaksymic <ryanmaksymic@gmail.com>
2018-03-14 17:57:43 -0400

    Added MiniPlayer storyboard. Tapping mini player modally presents the player view. Within the player view, tapping the dismissal chevron or swiping down dismisses the view.

--- Podclips/View Controllers/MiniPlayerViewController.swift
+++ Podclips/View Controllers/MiniPlayerViewController.swift
@@ -9,27 +9,38 @@
 import UIKit
 
 class MiniPlayerViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  // MARK: - Outlets
+  
+  @IBOutlet weak var artworkImageView: UIImageView!
+  @IBOutlet weak var episodeNameLabel: UILabel!
+  @IBOutlet weak var podcastNameLabel: UILabel!
+  @IBOutlet weak var detailsLabel: UILabel!
+  @IBOutlet weak var playPauseButton: UIButton!
+  
+  
+  // MARK: - Setup
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  // MARK: - Touches
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    print("Mini Player touch!")
+    performSegue(withIdentifier: "PresentPlayer", sender: nil)
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
 }
--- Podclips/View Controllers/PlayerViewController.swift
+++ Podclips/View Controllers/PlayerViewController.swift
@@ -9,19 +9,50 @@
 import UIKit
 
 class PlayerViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  // MARK: - Outlets
+  
+  @IBOutlet weak var artworkImageView: UIImageView!
+  @IBOutlet weak var episodeNameLabel: UILabel!
+  @IBOutlet weak var podcastNameLabel: UILabel!
+  @IBOutlet weak var detailsLabel: UILabel!
+  @IBOutlet weak var timeSlider: UISlider!
+  @IBOutlet weak var currentTimeLabel: UILabel!
+  @IBOutlet weak var totalTimeLabel: UILabel!
+  @IBOutlet weak var playPauseButton: UIButton!
+  
+  
+  // MARK: - Properties
+  
+  
+  // MARK: - Setup
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  
+  // MARK: - Actions
+  
+  @IBAction func dismiss(_ sender: UIButton) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  
+  // MARK: - Gestures
+  
+  @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
 }
