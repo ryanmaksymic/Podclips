999ef8dd4d385783f883c5c6f6f22b14ce4b8340
ryanmaksymic <ryanmaksymic@gmail.com>
2018-03-14 17:02:05 -0400

    Started setting up Favourites, Player, and Mini Player. Brought in button image assets.

---
+++ Podclips/View Controllers/MiniPlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MiniPlayerViewController.swift
+//  Podclips
+//
+//  Created by Ryan Maksymic on 2018-03-14.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import UIKit
+
+class MiniPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podclips/View Controllers/PlayerViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PlayerViewController.swift
+//  Podclips
+//
+//  Created by Ryan Maksymic on 2018-03-14.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
