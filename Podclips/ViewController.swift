3598a840a5546afa81b9721f4f464c1731e2d13e
ryanmaksymic <ryanmaksymic@gmail.com>
2018-03-14 14:19:28 -0400

    First commit

---
+++ Podclips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podclips
+//
+//  Created by Ryan Maksymic on 2018-03-14.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
