814e6e6107de73d2a9b21df25be728c9056c8aa4
ryanmaksymic <ryanmaksymic@gmail.com>
2018-03-15 16:41:03 -0400

    FavouritesTableViewController: Can delete tracks; removes from data store

--- Podclips/View Controllers/FavouritesTableViewController.swift
+++ Podclips/View Controllers/FavouritesTableViewController.swift
@@ -24,7 +24,7 @@ class FavouritesTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     loadTracks()
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    self.navigationItem.rightBarButtonItem = self.editButtonItem
   }
   
   
@@ -91,6 +91,7 @@ class FavouritesTableViewController: UITableViewController {
   
   // MARK: - UITableViewDataSource
   
+  // TODO: Organize into sections by podcast name (?)
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
@@ -119,23 +120,21 @@ class FavouritesTableViewController: UITableViewController {
   }
   
   // TODO: Tracks can be deleted
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      let track = tracks[indexPath.row]
+      guard DataManager.delete(object: track) else {
+        print("Error deleting track")
+        return
+      }
+      tracks.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+  }
   
   
   // MARK: - Segmented control
