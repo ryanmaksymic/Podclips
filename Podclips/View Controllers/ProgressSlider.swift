94dfb8370470e1238c2b149abab1ffe1c8c2ecb1
ryanmaksymic <ryanmaksymic@gmail.com>
2018-03-16 17:35:28 -0400

    ProgressSlider: Fine tuned constraints; responds to user interaction as intended!

--- Podclips/View Controllers/ProgressSlider.swift
+++ Podclips/View Controllers/ProgressSlider.swift
@@ -13,7 +13,15 @@ import UIKit
   // MARK: - Properties
   
   private var progressView: UIProgressView!
+  
   private var knob: UIImageView!
+  private var knobCenterXConstraint: NSLayoutConstraint!
+  private var leftHandle: UIImageView!
+  private var leftHandleCenterXConstraint: NSLayoutConstraint!
+  private var rightHandle: UIImageView!
+  private var rightHandleCenterXConstraint: NSLayoutConstraint!
+  private var editZone: UIImageView!
+  let handleWidth: CGFloat = 25
   
   private var _progress: Float = 0.0
   public var progress: Float {
@@ -22,12 +30,11 @@ import UIKit
     }
     set {
       setProgress(newValue)
-      knob.center.x = CGFloat(newValue) * self.frame.width
+      knob.center.x = handleWidth + CGFloat(newValue) * progressView.frame.width
     }
   }
   public var minimumProgress: Float = 0.0
   public var maximumProgress: Float = 1.0
-  
   
   private var _isInEditingMode = false
   public var isInEditingMode: Bool! {
@@ -37,11 +44,16 @@ import UIKit
     set {
       setIsInEditingMode(newValue)
       knob.isHidden = newValue
+      leftHandle.isHidden = !newValue
+      rightHandle.isHidden = !newValue
+      editZone.isHidden = !newValue
     }
   }
   
+  public var editingRange: (Float, Float)?  // TODO: Return distance between handles
   
-  // MARK: - Private methods
+  
+  // MARK: - Setter methods
   
   private func setProgress(_ value: Float) {
     if value != _progress {
@@ -75,6 +87,7 @@ import UIKit
   private func setupView() {
     setupProgressView()
     setupKnob()
+    setupEditor()
   }
   
   func setupProgressView() {
@@ -83,8 +96,8 @@ import UIKit
     progressView.translatesAutoresizingMaskIntoConstraints = false
     progressView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
     progressView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    progressView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-    progressView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    progressView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: +handleWidth).isActive = true
+    progressView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -handleWidth).isActive = true
   }
   
   func setupKnob() {
@@ -94,24 +107,79 @@ import UIKit
     knob.widthAnchor.constraint(equalToConstant: 15).isActive = true
     knob.heightAnchor.constraint(equalToConstant: 35).isActive = true
     knob.centerYAnchor.constraint(equalTo: progressView.centerYAnchor).isActive = true
-    knob.centerXAnchor.constraint(greaterThanOrEqualTo: progressView.leadingAnchor).isActive = true
-    knob.centerXAnchor.constraint(lessThanOrEqualTo: progressView.trailingAnchor).isActive = true
+    knobCenterXConstraint = knob.centerXAnchor.constraint(equalTo: progressView.leftAnchor, constant: 0)
+    knobCenterXConstraint.isActive = true
     
     let knobPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
     knob.addGestureRecognizer(knobPanGestureRecognizer)
-    
     knob.isUserInteractionEnabled = true
+    
+    knob.isHidden = false
+  }
+  
+  func setupEditor() {
+    leftHandle = UIImageView(image: UIImage(named: "handle"))
+    addSubview(leftHandle)
+    leftHandle.translatesAutoresizingMaskIntoConstraints = false
+    leftHandle.widthAnchor.constraint(equalToConstant: handleWidth).isActive = true
+    leftHandle.heightAnchor.constraint(equalToConstant: 35).isActive = true
+    leftHandle.centerYAnchor.constraint(equalTo: progressView.centerYAnchor).isActive = true
+    leftHandleCenterXConstraint = leftHandle.centerXAnchor.constraint(equalTo: progressView.leftAnchor, constant: 0)
+    leftHandleCenterXConstraint.isActive = true
+    
+    let leftHandlePanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+    leftHandle.addGestureRecognizer(leftHandlePanGestureRecognizer)
+    leftHandle.isUserInteractionEnabled = true
+    
+    rightHandle = UIImageView(image: UIImage(named: "handle"))
+    addSubview(rightHandle)
+    rightHandle.translatesAutoresizingMaskIntoConstraints = false
+    rightHandle.widthAnchor.constraint(equalToConstant: handleWidth).isActive = true
+    rightHandle.heightAnchor.constraint(equalToConstant: 35).isActive = true
+    rightHandle.centerYAnchor.constraint(equalTo: progressView.centerYAnchor).isActive = true
+    rightHandleCenterXConstraint = rightHandle.centerXAnchor.constraint(equalTo: progressView.leftAnchor, constant: 200)
+    rightHandleCenterXConstraint.isActive = true
+    
+    let rightHandlePanGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+    rightHandle.addGestureRecognizer(rightHandlePanGestureRecognizer)
+    rightHandle.isUserInteractionEnabled = true
+    
+    editZone = UIImageView(image: UIImage(named: "editZone"))
+    addSubview(editZone)
+    sendSubview(toBack: editZone)
+    editZone.translatesAutoresizingMaskIntoConstraints = false
+    editZone.heightAnchor.constraint(equalToConstant: 10).isActive = true
+    editZone.centerYAnchor.constraint(equalTo: progressView.centerYAnchor).isActive = true
+    editZone.leftAnchor.constraint(equalTo: leftHandle.centerXAnchor, constant: handleWidth/2).isActive = true
+    editZone.rightAnchor.constraint(equalTo: rightHandle.centerXAnchor, constant: -handleWidth/2).isActive = true
+    
+    leftHandle.isHidden = true
+    rightHandle.isHidden = true
+    editZone.isHidden = true
   }
   
   
   // MARK: - Gestures
   
+  // TODO: Clean all of this up
   @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
     let touchLocation = recognizer.location(in: self)
-    if touchLocation.x > 0 && touchLocation.x < self.frame.width {
-      recognizer.view!.center.x = touchLocation.x
-      setProgress(Float(touchLocation.x/self.frame.width))
+    if recognizer.view == knob {
+      if touchLocation.x > handleWidth && touchLocation.x < self.frame.width - handleWidth {
+        setProgress(Float((touchLocation.x - handleWidth)/progressView.frame.width))
+        knobCenterXConstraint.constant = touchLocation.x - handleWidth
+      }
+      sendActions(for: .valueChanged)
     }
-    sendActions(for: .valueChanged)
+    else if recognizer.view == leftHandle {
+      if touchLocation.x > 0 + handleWidth/2 && touchLocation.x < rightHandle.center.x - handleWidth {
+        leftHandleCenterXConstraint.constant = touchLocation.x - handleWidth
+      }
+    }
+    else if recognizer.view == rightHandle {
+      if touchLocation.x < self.frame.width - handleWidth/2 && touchLocation.x > leftHandle.center.x + handleWidth {
+        rightHandleCenterXConstraint.constant = touchLocation.x - handleWidth
+      }
+    }
   }
 }
