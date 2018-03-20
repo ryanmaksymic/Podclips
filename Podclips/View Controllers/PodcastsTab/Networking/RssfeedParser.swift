8df5ba7a12f34aa0da68751dff2c7c064e59a5d5
yongwoohuh <yongwoo.huh@me.com>
2018-03-20 14:58:43 -0400

    make parser file

---
+++ Podclips/View Controllers/PodcastsTab/Networking/RssfeedParser.swift
@@ -0,0 +1,10 @@
+//
+//  RssfeedParser.swift
+//  Podclips
+//
+//  Created by Yongwoo Huh on 2018-03-20.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import Foundation
+import FeedKit
